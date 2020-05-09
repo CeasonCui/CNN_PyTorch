463b32ec5c0497f5cf7c6877ed69b938b36afd3e
Ceason Cui <ceasoncui@gmail.com>
2020-05-09 15:43:45 +0900

    dataset delete

--- _1_text.py
+++ _1_text.py
@@ -1,10 +1,10 @@
 import torch
 import numpy as np
 
-np_data = np.arange(6).reshape((2,3))
-torch_data = torch.from_numpy(np_data)
+data = [-1,-2,1,2]
+tensor = torch.FloatTensor(data) 
 
 print(
-    '\nnumpy', np_data,
-    '\ntorch', torch_data,
-)
+    '\nnumpy', np.abs(data),
+    '\ntorch', torch.abs(tensor)
+) 
