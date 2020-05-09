17039dd2c554590aaa7ac789905a8298e32c17b3
Ceason Cui <ceasoncui@gmail.com>
2020-05-10 00:14:58 +0900

    channel=32 cnn=3

--- _3_cnn_okado.py
+++ _3_cnn_okado.py
@@ -19,7 +19,7 @@ EPOCH = 10               # train the training data n times, to save time, we jus
 BATCH_SIZE = 32
 LR = 0.001              # learning rate
 DOWNLOAD_MNIST = False
-channel = 1
+channel = 32
 
 class MyDataset(Dataset):
     def __init__(self, root_dir, img, transform=None): #__init__是初始化该类的一些基础参数
@@ -117,7 +117,7 @@ class CNN(nn.Module):
         #     nn.ReLU(),                      # activation
         #     nn.MaxPool2d(2),                # output shape (32, 7, 7)
         # )
-        self.fc1 = nn.Linear(channel*8 * 4 * 4, 2)   # fully connected layer, output 2 classes
+        self.fc1 = nn.Linear(channel*4 * 8 * 8, 2)   # fully connected layer, output 2 classes
     
     def forward(self, x):
         x = x.float()
@@ -126,7 +126,7 @@ class CNN(nn.Module):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
-        x = self.conv4(x)
+        #x = self.conv4(x)
         x = x.view(x.size(0), -1)           # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
         output = self.fc1(x)
         #output = self.softmax(x)
