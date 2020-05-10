93cc9fe6df3a8ea8419038f0f76b93717a79941d
Ceason Cui <ceasoncui@gmail.com>
2020-05-10 14:02:25 +0900

    e,2,3

--- _3_cnn_okado.py
+++ _3_cnn_okado.py
@@ -19,7 +19,7 @@ EPOCH = 100               # train the training data n times, to save time, we ju
 BATCH_SIZE = 32
 LR = 0.001              # learning rate
 DOWNLOAD_MNIST = False
-channel = 4
+channel = 2
 
 class MyDataset(Dataset):
     def __init__(self, root_dir, img, transform=None): #__init__是初始化该类的一些基础参数
