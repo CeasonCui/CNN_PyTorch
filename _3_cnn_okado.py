14740090788db6ebc264f14b13c374f925c0789f
Ceason Cui <ceasoncui@gmail.com>
2020-05-11 17:43:58 +0900

    feature map save

--- _3_cnn_okado.py
+++ _3_cnn_okado.py
@@ -1,6 +1,7 @@
 import os
 
 # third-party library
+import cv2
 import torch
 import torch.nn as nn
 import torchvision
@@ -15,11 +16,11 @@ from torchvision.utils import make_grid
 # torch.manual_seed(1)    # reproducible
 
 # Hyper Parameters
-EPOCH = 100               # train the training data n times, to save time, we just train 1 epoch
+EPOCH = 1               # train the training data n times, to save time, we just train 1 epoch
 BATCH_SIZE = 32
 LR = 0.001              # learning rate
 DOWNLOAD_MNIST = False
-channel = 32
+channel = 8
 
 
 
@@ -261,3 +262,61 @@ plt.ylabel('ACCURACY')
 plt.legend(['train acc', 'test acc'])
 plt.title('accuracy')
 plt.savefig("accuracy_image.png")
+
+
+class FeatureVisualization():
+    def __init__(self,img_path,selected_layer,model):
+        self.img_path=img_path
+        self.selected_layer=selected_layer
+        self.pretrained_model = model.features
+
+    def process_image(self):
+        img=cv2.imread(self.img_path)
+        #img=preprocess_image(img)
+        return img
+
+    def get_feature(self):
+        # input = Variable(torch.randn(1, 3, 224, 224))
+        input=self.process_image()
+        print(input.shape)
+        x=input
+        for index,layer in enumerate(self.pretrained_model):
+            x=layer(x)
+            if (index == self.selected_layer):
+                return x
+
+    def get_single_feature(self):
+        features=self.get_feature()
+        print(features.shape)
+
+        feature=features[:,0,:,:]
+        print(feature.shape)
+
+        feature=feature.view(feature.shape[1],feature.shape[2])
+        print(feature.shape)
+
+        return feature
+
+    def save_feature_to_img(self):
+        #to numpy
+        feature=self.get_single_feature()
+        feature=feature.data.numpy()
+
+        #use sigmod to [0,1]
+        feature= 1.0/(1+np.exp(-1*feature))
+
+        # to [0,255]
+        feature=np.round(feature*255)
+        print(feature[0])
+
+        cv2.imwrite('./feature_img.jpg',feature)
+
+
+
+
+if __name__=='__main__':
+    # get class
+    myClass=FeatureVisualization('./dataset_6/dataset/square_d2_p1378.jpg',5,cnn)
+    print (myClass.pretrained_model)
+
+    myClass.save_feature_to_img()
