76fc08bfc794c5a75d82c6ca329513c76b86510c
Ceason Cui <ceasoncui@gmail.com>
2020-05-13 00:45:17 +0900

    debug

--- _5_cnn_acc_check.py
+++ _5_cnn_acc_check.py
@@ -96,7 +96,7 @@ class CNN(nn.Module):
             nn.ReLU(),                      # activation
             nn.MaxPool2d(2),                # output shape (256, 4, 4)
         )
-        self.fc1 = nn.Linear(channel*4 * 8 * 8, 5)   # fully connected layer, output 2 classes
+        self.fc1 = nn.Linear(channel*4 * 8 * 8, 2)   # fully connected layer, output 2 classes
         self.pool = nn.MaxPool2d(2, stride=2)
         self.relu = nn.ReLU()
     def forward(self, x):
